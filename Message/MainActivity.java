8e85faca1ec02dd5d2f6757685a85d5fb4f1f2ed
Omkar <122523386+Omkar11Shirke@users.noreply.github.com>
2023-02-23 23:33:31 +0530

    Add files via upload

---
+++ Message/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.messagingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
